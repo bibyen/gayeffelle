4c2177b0d10694cade1fb56159bb2f01f904daaa
bautistv <vbf.bautista@gmail.com>
2026-08-31 21:54:40 +1000

    add package stuff

--- frontend/src/mockPlayers.ts
+++ frontend/src/mockPlayers.ts
@@ -7,5 +7,7 @@ export interface Player {
 export const MOCK_PLAYERS: Player[] = [
   { player_id: "1", first_name: "John", last_name: "Smith" },
   { player_id: "2", first_name: "Sarah", last_name: "Jones" },
-  { player_id: "3", first_name: "David", last_name: "Miller" }
+  { player_id: "3", first_name: "David", last_name: "Miller" },
 ];
+
+console.debug(MOCK_PLAYERS);
